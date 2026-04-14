01ba76df73041970bf3d8f5071a86a95dfdd8b22
d-beezee <59012086+d-beezee@users.noreply.github.com>
2026-04-14 13:18:43 +0200

    feat: Add double parallax effect

--- Experiments/CucuccExperiments.gd
+++ Experiments/CucuccExperiments.gd
@@ -1,21 +1,32 @@
 extends Node2D
 
-var is_blocked = false
+var view_angle := 0.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var step := 1.0
+@export var pano_width := 7680.0
+@export var fg_width := 8000.0
 
+@onready var bg := $Background
+@onready var fg := $Foreground
+@onready var rooms = $Rooms.get_children()
 
-func _process(delta):
+func _ready():
+	update_layers()
+
+func _process(_delta: float) -> void:
 	var axis = Input.get_axis("ui_left","ui_right")
-	
-	if $Camera2D.position.x > 1920 && axis >= 1:
-		return
-	
-	$Camera2D.position.x += axis * 500 * delta
+	view_angle = wrapf(view_angle + axis * step, 0.0, 360.0)
+	update_layers()
 
 
-func _unhandled_input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		is_blocked = true
+	for chunk in rooms:
+		chunk.position.x = fmod(chunk.position.x - axis * step / 360.0 * pano_width ,pano_width )
+		
+
+
+func angle_to_offset(angle: float, width: float) -> float:
+	return (angle / 360.0) * width
+
+func update_layers():
+	bg.screen_offset.x = angle_to_offset(view_angle, pano_width)
+	fg.screen_offset.x = angle_to_offset(view_angle, fg_width)
