927e04d2ff23996191b72e5d3a206b21a4937020
kotrodenx <mengiowork@gmail.com>
2026-04-16 16:43:27 +0200

    fix: fire check on different states of wood logs and on placing

--- Items/wood_logs.gd
+++ Items/wood_logs.gd
@@ -1,12 +1,16 @@
 extends DraggableItem
 class_name WoodLogs
 
+signal fire_lit
 var placed = false
 
 func drag_item():
 	if !placed:
 		super()
 
+func check_fire():
+	if placed and item.id == "wood_logs_fire":
+		fire_lit.emit()
+
 func use_after():
-	if item.id == "wood_logs_fire":
-		App.game_status.fire_lit = true
+	check_fire()
--- Rooms/Room2/fireplace.gd
+++ Rooms/Room2/fireplace.gd
@@ -5,5 +5,10 @@ var logs : WoodLogs = null
 func _on_item_dropped(item : DraggableItem):
 	if item is WoodLogs:
 		logs = item
-		item.placed = true
-		item.global_position = global_position - (item.texture_normal.get_size() * item.scale) / 2
+		logs.placed = true
+		logs.global_position = global_position - (item.texture_normal.get_size() * item.scale) / 2
+		logs.fire_lit.connect(_on_fire_lit)
+		logs.check_fire()
+
+func _on_fire_lit():
+	App.game_status.fire_lit = true
