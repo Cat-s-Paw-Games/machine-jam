ec1cde49063e3f9f4cd958d15f706eb2e8f64197
d-beezee <59012086+d-beezee@users.noreply.github.com>
2026-04-16 09:00:41 +0200

    feat: Add inventory button

--- services/event_service.gd
+++ services/event_service.gd
@@ -2,6 +2,7 @@ extends Node
 class_name EventService
 
 signal switch_lights_on()
+signal inventory_enabled()
 
 func setup():
 	switch_lights_on.connect(_on_switch_lights)
@@ -12,4 +13,4 @@ func _on_switch_lights():
 	await App.show_popup("[color=#f00][b]DANGER! [pause]DANGER! [pause]DANGER![pause][/b][/color]",true)
 	await App.show_popup("[color=#f00]UNIDENTIFIED ORGANIC STRUCTURE[/color]",true)
 	await App.show_popup("Human™[pause] your presence here is.[pause=0.5].[pause=0.5].[pause=0.5]\nUnexpected. [pause] Please stand by as our systems find a resolution.",true)
-	
+	App.events.inventory_enabled.emit()
---
+++ ui.gd
@@ -0,0 +1,16 @@
+extends CanvasLayer
+
+var inventory_open = false
+
+func _ready() -> void:
+	App.events.inventory_enabled.connect(func(): %InventoryButton.visible = true)
+
+func _on_inventory_button_pressed() -> void:
+	if inventory_open: 
+		inventory_open = false
+		App.navigation_enabled = true
+		UIAnimation.animate_slide_to_top($Inventory)
+	else: 
+		inventory_open = true
+		App.navigation_enabled = false
+		UIAnimation.animate_slide_from_top($Inventory)
