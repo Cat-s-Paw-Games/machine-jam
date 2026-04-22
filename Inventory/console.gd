53323160ebd96a4f376167127f94e2264be415df
kotrodenx <mengiowork@gmail.com>
2026-04-22 17:49:14 +0200

    feat: console updated

--- Inventory/console.gd
+++ Inventory/console.gd
@@ -1,55 +1,58 @@
 extends PanelContainer
 class_name ConsoleMachine
 
+@onready var console = %ConsoleMenu
+@onready var inventory_slot: InventorySlot = %InventorySlot
+
 var current_item = null
 func _ready():
-	%InventorySlot.added_item_to_slot.connect(_on_item_add)
-	%InventorySlot.removed_item_from_slot.connect(_on_item_remove)
+	inventory_slot.added_item_to_slot.connect(_on_item_add)
+	inventory_slot.removed_item_from_slot.connect(_on_item_remove)
 
 func _on_item_add(item_id : String):
 	current_item = item_id
 	if item_id == "bound_relic_charged":
-		%Console.correct_item = true
+		console.correct_item = true
 	elif item_id == "floppy_card_game":
-		%Console.current_floppy = App.items[item_id]
+		console.current_floppy = App.items[item_id]
 		add_floppy_line()
 	elif item_id.contains("floppy_"):
-		%Console.floppy_log = true
-		%Console.current_floppy = App.items[item_id]
+		console.floppy_log = true
+		console.current_floppy = App.items[item_id]
 		add_floppy_line()
 	else:
-		%Console.floppy_log = false
-		%Console.current_floppy = null
+		console.floppy_log = false
+		console.current_floppy = null
 		remove_floppy_line()
-		%Console.correct_item = false
+		console.correct_item = false
 
-var floppy_menu_line = "> Copy Floppy Log"
-var supercharge_line = "> Install Supercharge protocol"
+var floppy_menu_line = "Copy Floppy Log"
+var supercharge_line = "Install Supercharge protocol"
 
 func add_floppy_line():
-	if %Console.current_floppy.id == "floppy_card_game":
-		if !%Console.screens["main_menu"]["lines"].has(supercharge_line):
-			%Console.screens["main_menu"]["lines"].append(supercharge_line)
-	elif %Console.floppy_log:
-		if !%Console.screens["main_menu"]["lines"].has(floppy_menu_line):
-			%Console.screens["main_menu"]["lines"].append(floppy_menu_line)
+	if console.current_floppy.id == "floppy_card_game":
+		if !console.screens["main_menu"]["lines"].has(supercharge_line):
+			console.screens["main_menu"]["lines"].append(supercharge_line)
+	elif console.floppy_log:
+		if !console.screens["main_menu"]["lines"].has(floppy_menu_line):
+			console.screens["main_menu"]["lines"].append(floppy_menu_line)
 	
-	%Console.render()
+	console.render()
 
 func remove_floppy_line():
-	if %Console.screens["main_menu"]["lines"].has(supercharge_line):
-		%Console.screens["main_menu"]["lines"].erase(supercharge_line)
+	if console.screens["main_menu"]["lines"].has(supercharge_line):
+		console.screens["main_menu"]["lines"].erase(supercharge_line)
 	
-	if %Console.screens["main_menu"]["lines"].has(floppy_menu_line):
-		%Console.screens["main_menu"]["lines"].erase(floppy_menu_line)
+	if console.screens["main_menu"]["lines"].has(floppy_menu_line):
+		console.screens["main_menu"]["lines"].erase(floppy_menu_line)
 	
-	%Console.render()
+	console.render()
 
 
 func _on_item_remove():
 	current_item = null
-	%Console.correct_item = false
-	%Console.floppy_log = false
+	console.correct_item = false
+	console.floppy_log = false
 	remove_floppy_line()
 
 func _on_close_pressed() -> void:
@@ -57,24 +60,23 @@ func _on_close_pressed() -> void:
 	App.ui.close_console()
 
 
-func _on_console_floppy_saved() -> void:
+func _on_card_game_won() -> void:
+	await get_tree().create_timer(1.0).timeout
+	if inventory_slot.item.id == "bound_relic":
+		inventory_slot.item = inventory_slot.item.transform()
+		inventory_slot.update_visual()
+		console.correct_item = true
+	%CardGame.hide()
+	console.show()
+
+
+func _on_console_menu_floppy_saved() -> void:
 	if current_item:
-		%InventorySlot.empty_slot()
+		inventory_slot.empty_slot()
 		current_item = null
 
 
-
-
-func _on_console_play_card_game() -> void:
-	if %InventorySlot.item && %InventorySlot.item.id == "bound_relic":
-		%Console.hide()
+func _on_console_menu_play_card_game() -> void:
+	if inventory_slot.item && inventory_slot.item.id == "bound_relic":
+		console.hide()
 		%CardGame.show()
-
-func _on_card_game_won() -> void:
-	await get_tree().create_timer(1.0).timeout
-	if %InventorySlot.item.id == "bound_relic":
-		%InventorySlot.item = %InventorySlot.item.transform()
-		%InventorySlot.update_visual()
-		%Console.correct_item = true
-	%CardGame.hide()
-	%Console.show()
---
+++ Inventory/console_menu.gd
@@ -0,0 +1,155 @@
+extends VBoxContainer
+
+signal floppy_saved
+signal play_card_game
+
+@export var main_screen : Node = null
+
+var paused = false
+var correct_item = false
+var floppy_log = false
+var current_floppy = null
+var background = "#3c630f"
+var foreground = "#ffffff"
+var current_line = 0;
+var screens = {
+	"main_menu":  {
+		"lines": [
+			"[EMERGENCY HATCH] Open",
+			"Logs"
+		],
+	},
+	"open_fail":  {
+		"lines": [
+			"Back"
+		],
+	},
+	"open_success":  {
+	},
+	"logs": {
+		"lines": [
+			"Back",
+		],
+	}
+}
+var current_screen = "main_menu"
+
+var lines: 
+	get():
+		return screens[current_screen].get("lines",[])
+var pretext:
+	get():
+		return screens[current_screen].get("pretext","")
+
+func get_next_screen():
+	match current_screen:
+		"main_menu":
+			if current_line == 0 and not correct_item: return "open_fail"
+			if current_line == 0 and correct_item: return "open_success"
+			elif current_line == 1: return "logs"
+			elif current_line == 2 and !App.game_status.card_game_uploaded and !floppy_log:
+				install_card_minigame()
+			elif current_line == 2 and App.game_status.card_game_uploaded:
+				play_card_game.emit()
+			elif (current_line == 2 and !App.game_status.card_game_uploaded or current_line == 3) and floppy_log:
+				save_floppy_data()
+				var keys = App.game_status.console_logs.keys()
+				return keys[keys.size() - 1]
+		"open_fail":
+			if current_line == 0: return "main_menu"
+		"logs":
+			if current_line == 0: return "main_menu"
+			else:
+				return App.game_status.console_logs.keys()[current_line - 1]
+		_:
+			if current_line == 0: return "logs"
+
+func install_card_minigame():
+	App.game_status.card_game_uploaded = true
+	current_floppy = null
+	floppy_saved.emit()
+	load_data()
+
+func save_floppy_data():
+	if floppy_log && current_floppy != null:
+		App.game_status.console_logs[current_floppy.id] = {}
+		var pretext_start = current_floppy.description.find("[pretext]")
+		var pretext_end = current_floppy.description.find("[/pretext]")
+		var _pretext = current_floppy.description.substr(pretext_start, pretext_end + "[/pretext]".length())
+		_pretext = _pretext.replace("[pretext]", "").replace("[/pretext]", "")
+		
+		var content_start = current_floppy.description.find("[content]")
+		var content_end = current_floppy.description.find("[/content]")
+		var _content = current_floppy.description.substr(content_start, content_end + "[/content]".length())
+		_content = _content.replace("[content]", "").replace("[/content]", "").replace("[MACHINE_NAME]", App.MACHINE_NAME)
+		App.game_status.console_logs[current_floppy.id] = {"line": current_floppy.name, "pretext": _pretext + "\n" + _content}
+	
+	floppy_log = false
+	current_floppy = null
+	load_data()
+	main_screen.load_data()
+	floppy_saved.emit()
+
+func _ready() -> void:
+	load_data()
+	render()
+
+func load_data():
+	var supercharge_line = "Supercharge"
+	if App.game_status.card_game_uploaded && !screens["main_menu"]["lines"].has(supercharge_line):
+		screens["main_menu"]["lines"].append(supercharge_line)
+	
+	var back_line = "Back"
+	screens["logs"]["lines"].clear()
+	screens["logs"]["lines"].append(back_line)
+	for log_id in App.game_status.console_logs:
+		screens[log_id] = App.game_status.console_logs[log_id]
+		screens[log_id]["lines"] = []
+		screens[log_id]["lines"].append("Back")
+		screens["logs"]["lines"].append(App.game_status.console_logs[log_id].line)
+
+func create_label(text : String) -> Label:
+	var label : Label = Label.new()
+	label.add_theme_font_size_override("font_size", 18)
+	label.set_meta("text", text)
+	label.text = text
+	return label
+	
+func render():
+	for c in get_children():
+		c.queue_free()
+	
+	var i = 0
+	for l in lines:
+		var label = create_label(l)
+		label.text = "> %s" % l if i == current_line else l
+		i += 1
+		add_child(label)
+	if current_screen == "open_success":
+		paused = true
+		await get_tree().create_timer(3).timeout
+		App.events._on_end_game()
+
+func _input(event: InputEvent) -> void:
+	if paused: return
+	if event is InputEventKey and event.pressed:
+		match event.keycode:
+			KEY_DOWN:
+				if lines.size() > 0:
+					current_line = (current_line + 1) % lines.size()
+					render()
+			KEY_UP:
+				if lines.size() > 0:
+					current_line = (current_line - 1 + lines.size()) % lines.size()
+					render()
+			KEY_ENTER:
+				var next_screen = get_next_screen()
+				if next_screen:
+					current_screen = next_screen
+				else:
+					current_screen = "main_menu"
+				current_line = 0
+				render()
+				if main_screen:
+					main_screen.current_screen = current_screen
+					main_screen.render()
