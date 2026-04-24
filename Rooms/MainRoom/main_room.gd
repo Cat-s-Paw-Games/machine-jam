910b335505ec96af6b3bf06624ea39a78e0c4b14
d-beezee <59012086+d-beezee@users.noreply.github.com>
2026-04-24 20:36:35 +0200

    fix: Audio and switch click

--- Rooms/MainRoom/main_room.gd
+++ Rooms/MainRoom/main_room.gd
@@ -4,5 +4,7 @@ func _ready():
 	$BG.queue_free()
 
 func _on_switch_pressed() -> void:
-	if not App.game_status.lights_on: 
-		App.ui.open_scene_in_focus(preload("res://Rooms/MainRoom/LightSwitchFocus.tscn").instantiate())
+	if App.game_status.lights_on: return
+	if App.in_focus: return
+	
+	App.ui.open_scene_in_focus(preload("res://Rooms/MainRoom/LightSwitchFocus.tscn").instantiate())
