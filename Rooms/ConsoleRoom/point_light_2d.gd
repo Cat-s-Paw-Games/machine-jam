d4a9ee0d7ecdb9cb114c0787c5c94a22bee344b2
d-beezee <59012086+d-beezee@users.noreply.github.com>
2026-04-24 18:35:39 +0200

    feat: Add face lights

--- Rooms/ConsoleRoom/point_light_2d.gd
+++ Rooms/ConsoleRoom/point_light_2d.gd
@@ -1,12 +1,35 @@
 extends PointLight2D
 
-func _ready() -> void:
-	App.events.switch_lights_on.connect(func(): enabled = true)
-	var tween = create_tween()
-	tween.set_loops() # loop infinito
+@export var energy_off_min = 0
+@export var energy_off_max = 0
+@export var energy_on_min = 1
+@export var energy_on_max = 1
+@export var loop_duration = 1
 
-	tween.tween_property(self, "energy", 4,3)\
+
+var light_up = false
+var light_loop
+
+
+func _ready() -> void:
+	App.events.switch_lights_on.connect(func(): 
+		stop_loop()
+		start_loop(energy_on_min,energy_off_max)
+	)
+	start_loop(energy_off_min,energy_off_max)
+
+
+func stop_loop():
+	light_loop.stop()
+	light_loop = null
+
+func start_loop(base_energy_min,base_energy_max):
+	light_loop = create_tween()
+	light_loop.set_loops() # loop infinito
+
+	light_loop.tween_property(self, "energy", base_energy_max ,loop_duration)\
 		.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
 
-	tween.tween_property(self, "energy", 0.4, 3)\
+	light_loop.tween_property(self, "energy", base_energy_min , loop_duration)\
 		.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
+	
