8fee8a1cbf937d88389fab07c87fce581d3b3d7c
d-beezee <59012086+d-beezee@users.noreply.github.com>
2026-04-25 12:37:28 +0200

    fix: Remove old audio handling and decrease sound

--- Items/wood_logs.gd
+++ Items/wood_logs.gd
@@ -10,7 +10,6 @@ func drag_item():
 
 func check_fire():
 	if placed and item.id == "wood_logs_fire":
-		App.audio.play("sfx", "res://assets/music/sfx/fire_whoosh.mp3")
 		$AudioStreamPlayer2D.play()
 		fire_lit.emit()
 
