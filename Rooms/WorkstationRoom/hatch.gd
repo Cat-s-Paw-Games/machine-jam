534eea4616629db93375fe2217934dd3931569c7
kotrodenx <mengiowork@gmail.com>
2026-04-25 02:56:50 +0200

    feat: hatch animation and hover hint clean on open

--- Rooms/WorkstationRoom/hatch.gd
+++ Rooms/WorkstationRoom/hatch.gd
@@ -6,7 +6,9 @@ func _ready() -> void:
 	App.events.hatch_open.connect(_on_hatch_open)
 
 func _on_hatch_open():
-	hatch_sprite.position.y -= 100
+	$Button/HoverHint.text = ""
+	var tween = create_tween()
+	tween.tween_property(hatch_sprite,"position:y", -50, .5)
 
 func _on_button_pressed() -> void:
 	if App.game_status.end_reached:
