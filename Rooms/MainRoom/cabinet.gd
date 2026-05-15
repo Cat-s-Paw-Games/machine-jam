90344568fa215c6d7c8f051f27700b99a38fc258
d-beezee <59012086+d-beezee@users.noreply.github.com>
2026-05-15 23:17:30 +0200

    fix: Move second code inside cabinet

--- Rooms/MainRoom/cabinet.gd
+++ Rooms/MainRoom/cabinet.gd
@@ -10,5 +10,8 @@ func _ready() -> void:
 			App.game_status.cabinet_open = true
 			var oscillator = preload("res://Items/oscillation_regulator.tscn").instantiate()
 			add_child(oscillator)
+			var floppy = preload("res://Items/floppy.tscn").instantiate()
+			floppy.position.x -= 100
+			add_child(floppy)
 			$Sprite2D.texture = preload("res://assets/images/cabinet_open.png")
 	)
